09ebb0a36ca606a386f79ea50e7f054cfee4e8ad
Tomas Szabo <bady.tomas33@gmail.com>
2023-10-05 13:02:32 +0100

    Add Advanced Movement

--- Assets/Resources/Scripts/AdvancedPlayerMovement.cs
+++ Assets/Resources/Scripts/AdvancedPlayerMovement.cs
@@ -2,29 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class AdvancedPlayerMovement : MonoBehaviour
 {
 
     public float speed = 10f;
     public float jumpHeight = 7f;
+    public float dashSpeed = 20f;
+    public float crouchHeight = .5f;
+    public LayerMask whatisGround;
+    public Transform groundCheckPoint;
+    public float groundCheckRadius = 0.2f;
+    public AudioClip jumpSound;
+    public AudioClip dashSound;
+    public AudioClip footstepSound;
     private Rigidbody2D body;
-    private Animator anim;
+    public Animator anim;
+    private AudioSource audioSource;
     private bool grounded;
+    private bool canDoubleJump = false;
+    private bool isDashing = false;
+    private bool isCrouching = false;
     private bool facingRight = true;
-    
-
-    void Awake()
+    // Start is called before the first frame update
+    private void Awake()
     {
         body = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();        
+        audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        grounded = Physics2D.OverlapCircle(groundCheckPoint.position, groundCheckRadius, whatisGround);
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         body.velocity = new Vector2(horizontalInput * speed, body.velocity.y);
-        anim.SetBool("walk", horizontalInput!=0);
+        anim.SetBool("walk", horizontalInput !=0);
 
         if ((horizontalInput>0 && !facingRight)||(horizontalInput<0 && facingRight))
         {
@@ -37,25 +50,18 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void Jump()
-    {
-        body.velocity = new Vector2(body.velocity.x, jumpHeight);
-        grounded = false;
-        anim.SetTrigger("Jump");
-    }
-
-    private void OnCollisionEnter2D(Collision2D other)
-    {
-        if (other.gameObject.CompareTag("Ground")) {
-            grounded = true;
-        }
-    }
-
     private void Flip()
     {
         Vector3 currentScale = gameObject.transform.localScale;
         currentScale.x *= -1;
         gameObject.transform.localScale = currentScale;
         facingRight = !facingRight;
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, jumpHeight);
+        anim.SetTrigger("Jump");
+        grounded = false;
     }
 }
